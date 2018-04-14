2c476056087669d02aac066c60493ed29cfe4786
Luca(Wei) Chen <z5155740@unsw.edu.au>
2018-04-14 17:35:30 +1000

    change default center of map

--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
  
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
 export default class Map extends Component {
   static defaultProps = {
-    center: {lat: 59.95, lng: 30.33},
-    zoom: 11
+    center: {lat: -33.92, lng: 151.23},
+    zoom: 15
   };
  
   render() {
@@ -16,11 +14,6 @@ export default class Map extends Component {
         defaultCenter={this.props.center}
         defaultZoom={this.props.zoom}
       >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text={'Siround'}
-        />
       </GoogleMapReact>
     );
   }
