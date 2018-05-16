1f092bc66988479ef489dcf73e1f783ef454128a
LINUX UBUNTU <maggie.zhu2016@outlook.com>
2018-05-16 10:23:15 +1000

    Enable Facebook and Twitter login. But cannot directly to the map page.

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wumu9g(ml2y_f5*!5(x6$hj6eih3qo3$e-)h$-uq+temb9x69s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['siround.com']
 
 
 # Application definition
