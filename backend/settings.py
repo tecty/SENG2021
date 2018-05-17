b46621a2de912535ce36af821cc92f96a8b38fa9
LINUX UBUNTU <maggie.zhu2016@outlook.com>
2018-05-17 15:34:52 +1000

    save

--- backend/settings.py
+++ backend/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.weixin',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
